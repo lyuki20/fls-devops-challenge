73fa1500cecb1c3c82260531ff8990b68f518b12
Luca Tsutsumi <70017234+lyuki20@users.noreply.github.com>
2025-02-01 21:51:16 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,6 +36,8 @@ resource "aws_s3_bucket_public_access_block" "website_public_block" {
 resource "aws_s3_bucket_policy" "website_policy_apply" {
   bucket = aws_s3_bucket.website.id
   policy = data.aws_iam_policy_document.website_policy.json
+  depends_on = [aws_s3_bucket_public_access_block.website_public_block]
+
 }
 
 data "aws_iam_policy_document" "website_policy" {
